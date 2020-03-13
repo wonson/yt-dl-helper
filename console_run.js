273e998477c8d8e3087151db9054ee1f73f00485
WonsonTheFantastic <wonson@gmail.com>
2020-03-13 19:18:01 +0800

    Create console_run.js

---
+++ console_run.js
@@ -0,0 +1,11 @@
+{
+	var videoinfolist =(JSON.parse(ytplayer.config.args.player_response)).streamingData.formats; 
+	var mosthdvideo = videoinfolist.reduce((a, b) => {
+		return a.height > b.height ? a : b;
+	});
+	var a = document.createElement("a");
+	a.href = mosthdvideo.url;
+	a.download = (JSON.parse(ytplayer.config.args.player_response)).videoDetails.title;
+	document.body.appendChild(a);
+	a.click()
+}
