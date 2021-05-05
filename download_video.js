5353a50cdd8ad2c5bedf9dec3d40219daed08b51
WonsonTheFantastic <wonson@gmail.com>
2021-05-05 16:56:25 +0800

    Update download_video.js
    Fix for yt update

--- download_video.js
+++ download_video.js
@@ -1,10 +1,11 @@
 {
-	var videoinfolist =(JSON.parse(ytplayer.config.args.player_response)).streamingData.formats; 
+	var r = ytplayer.config.args.raw_player_response;
+	var videoinfolist = r.streamingData.formats;
 	var mosthdvideo = videoinfolist.reduce((a, b) => {
 		return a.height > b.height ? a : b;
 	});
 	var a = document.createElement("a");
 	a.href = mosthdvideo.url;
-	a.download = (JSON.parse(ytplayer.config.args.player_response)).videoDetails.title;
+	a.download = r.videoDetails.title;
 	a.click();
 }
