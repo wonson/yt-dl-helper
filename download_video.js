bdc5b1024e876bed37eab227f8c76d2419e57e75
WonsonTheFantastic <wonson@gmail.com>
2021-05-05 20:25:36 +0800

    Update download_video.js

--- download_video.js
+++ download_video.js
@@ -1,20 +1,27 @@
 javascript:{
-	var r = ytplayer.config.args.raw_player_response;
-	var videoinfolist = r.streamingData.formats;
-	var mosthdvideo = videoinfolist.reduce((a, b) => {
-		return a.height > b.height ? a : b;
-	});
+	function getvideo(browserurl) {
+		var r = ytplayer.config.args.raw_player_response;
+		var videoinfolist = r.streamingData.formats;
+		var mosthdvideo = videoinfolist.reduce((a, b) => {
+			return a.height > b.height ? a : b;
+		});
+		return mosthdvideo
+	}
 
-	var a = document.createElement("a");
-    	var objurl = window.URL.createObjectURL(
-		    await fetch(mosthdvideo.url).
-            then(r => r.blob()).
-            then(blobFile => 
-                new File([blobFile], r.videoDetails.title, { type: mosthdvideo.mimeType })
-            )
-        )
-	a.href = objurl;
-	a.download = r.videoDetails.title;
-	a.click();
-    	a.remove();
+	async function dlvideo(video) {
+		var a = document.createElement("a");
+			var objurl = window.URL.createObjectURL(
+				await fetch(video.url).
+				then(r => r.blob()).
+				then(blobFile => 
+					new File([blobFile], r.videoDetails.title, { type: video.mimeType })
+				)
+			)
+		a.href = objurl;
+		a.download = r.videoDetails.title;
+		a.click();
+		a.remove();
+	}
+
+	dlvideo(getvideo())
 }
