c6fe634f34519895025b7849caabddee73681b11
WonsonTheFantastic <wonson@gmail.com>
2021-05-06 00:03:34 +0800

    Update download_video.js

--- download_video.js
+++ download_video.js
@@ -5,20 +5,33 @@ javascript:{
 		var mosthdvideo = videoinfolist.reduce((a, b) => {
 			return a.height > b.height ? a : b;
 		});
+		mosthdvideo["title"] = r.videoDetails.title;
 		return mosthdvideo
 	}
 
 	async function dlvideo(video) {
+		function cipherurl() {
+			// I surrender
+			var msg = "Not working for video.cipher or video.signatureCipher"
+			alert(msg)
+			throw msg
+		}
+
+		var url = video.url || cipherurl()
+		if (!url) {
+			return
+		}
+
 		var a = document.createElement("a");
 			var objurl = window.URL.createObjectURL(
-				await fetch(video.url).
+				await fetch(url).
 				then(r => r.blob()).
 				then(blobFile => 
-					new File([blobFile], r.videoDetails.title, { type: video.mimeType })
+					new File([blobFile], video.title, { type: video.mimeType })
 				)
 			)
 		a.href = objurl;
-		a.download = r.videoDetails.title;
+		a.download = video.title;
 		a.click();
 		a.remove();
 	}
